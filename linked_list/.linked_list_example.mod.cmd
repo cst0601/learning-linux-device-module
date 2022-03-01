65c6d7cf14f95304ee1a839210b48d3b18a1088d
cst0601 <chikuma@gmail.com>
2022-03-01 02:09:33 +0000

    clean

--- linked_list/.linked_list_example.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/chikuma/tutorials/linked_list/linked_list_example.mod := { echo  /home/chikuma/tutorials/linked_list/linked_list_example.o;  echo; } > /home/chikuma/tutorials/linked_list/linked_list_example.mod
--- linked_list/linked_list_example.mod
+++
@@ -1,2 +0,0 @@
-/home/chikuma/tutorials/linked_list/linked_list_example.o
-
